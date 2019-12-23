507713b34da11e844df26ee3531ec2ee95bf10e1
jovan998 <jovan.milic7@hotmail.com>
2019-12-23 21:41:40 +0100

    dodato osvetljenje, iscrtane rupe i palica, izmenjene boje

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ static int window_width, window_height;
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display(void);
+static void drawCircles(void);
+static void drawBat(void);
 
 int main(int argc, char** argv){
     
@@ -22,6 +24,23 @@ int main(int argc, char** argv){
    
     glMatrixMode(GL_PROJECTION);
     gluPerspective(70, 800.0/600.0, 0.1, 250);
+    
+    glClearColor(0.4, 0.4, 0.4, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    
+    float light_position[] = {-1, 1, 1, 0};
+    float light_ambient[] = {.3f, .3f, .3f, 1};
+    float light_diffuse[] = {.7f, .7f, .7f, 1};
+    float light_specular[] = {.7f, .7f, .7f, 1};
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    
 
     glutMainLoop();
     
@@ -38,10 +57,96 @@ static void on_keyboard(unsigned char key, int x, int y){
     }
 }
 
+void draw_axis(float len) {
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(len,0,0);
+
+        glColor3f(0,1,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,len,0);
+
+        glColor3f(0,0,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,len);
+    glEnd();
+}
+
 static void on_reshape(int width, int height)
 {
     window_width = width;
     window_height = height;
+}
+
+
+static void drawCircles(void){
+    
+    //odsecanje
+    GLdouble plane0[] = {0, -1, 0, 0};
+    glEnable(GL_CLIP_PLANE0);
+    glClipPlane(GL_CLIP_PLANE0, plane0);
+    
+    
+    float r = 0.5;
+    float spacing = 1.6/4.0;
+    
+    GLUquadricObj *qobj = gluNewQuadric();
+        
+    glPushMatrix();
+        //iscrtavanje rupa 3-1
+        glTranslatef(-2.3 + r + spacing, 0, -2.3 + r + spacing);
+        glColor3f(1, 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        glTranslatef(2*r + spacing, 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        glTranslatef(2*r + spacing, 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        
+        //iscrtavanje rupa 4-6
+        glTranslatef(0, 0, 2*r + spacing);
+        gluSphere(qobj, r, 30, 30);
+        
+        glTranslatef(-(2*r + spacing), 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        glTranslatef(-(2*r + spacing), 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        //iscrtavanje rupa 9-7
+        glTranslatef(0, 0, 2*r + spacing);
+        gluSphere(qobj, r, 30, 30);
+        
+        glTranslatef(2*r + spacing, 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        glTranslatef(2*r + spacing, 0, 0);
+        gluSphere(qobj, r, 30, 30);
+        
+        gluDeleteQuadric(qobj);
+    glPopMatrix();
+    
+    glDisable(GL_CLIP_PLANE0);
+
+}
+
+void drawBat(void){
+    
+    glPushMatrix();
+    
+        GLUquadricObj *qobj = gluNewQuadric();
+    
+        glTranslatef(-1, 0, -3.3);
+        glRotatef(-40, 0, 1, 0);
+
+        gluCylinder(qobj, 0.1, 0.3, 4, 30, 30);
+            
+        gluDeleteQuadric(qobj);
+    glPopMatrix();
+    
 }
 
 static void on_display(void){
@@ -51,19 +156,61 @@ static void on_display(void){
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0, 3, -3, 
+    gluLookAt(0, 5, -3.5, 
               0, 0, 0,
               0, 1, 0);
     
-    //iscrtavanje platforme
-    glColor3f(0, 0, 1);
-    glBegin(GL_QUADS);
-        glVertex3f(-2, 0, -2);
-        glVertex3f(2, 0, -2);
-        glVertex3f(2, 0, 2);
-        glVertex3f(-2, 0, 2);
-    glEnd();
+   // draw_axis(5);
     
+    //iscrtavanje platforme
+    glPushMatrix();
+        GLfloat ambient3[] = {1,0.78,0.35,0};
+        GLfloat diffuse3[] = {0.3,0.3,0,0,0};
+        GLfloat specular3[] = {0.6,0.6,0.6,0};
+        GLfloat shininess3 = 80;
+
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient3);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse3);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular3);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess3);
+        glBegin(GL_QUADS);
+            glVertex3f(-2.3, 0, -2.3);
+            glVertex3f(2.3, 0, -2.3);
+            glVertex3f(2.3, 0, 2.3);
+            glVertex3f(-2.3, 0, 2.3);
+        glEnd();
+    glPopMatrix();
+    
+    //iscrtavanje krugova
+    glPushMatrix();
+        GLfloat ambient2[] = {1,0.62,0.11,0};
+        GLfloat diffuse2[] = {0.2,0.2,0,0,0};
+        GLfloat specular2[] = {0.3,0.3,0.3,0};
+        GLfloat shininess2 = 40;
+
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient2);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse2);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular2);
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess2);
+        
+        drawCircles();
+    glPopMatrix();
+    
+       
+    //iscrtavanje palice
+    glPushMatrix();
+        GLfloat ambient1[] = {0.4,0.2,0.2,0};
+        GLfloat diffuse1[] = {0.3,0.3,0,0,0};
+        GLfloat specular1[] = {0.6,0.6,0.6,0};
+        GLfloat shininess1 = 80;
+
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient1);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse1);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, specular1);
+        glMaterialf(GL_FRONT, GL_SHININESS, shininess1);
+        
+        drawBat();
+    glPopMatrix();
     
     glutSwapBuffers();
 }
